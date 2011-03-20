06dba077d8231d6219b65254c38f2272dbd048fb
Koushik Dutta <koushd@gmail.com>
2011-03-20 12:51:07 -0700

    Fix build for slayher.
    Change-Id: Ie57041851cdcb3f5f14a799b017625cc9f1f2322

--- device_mecha.mk
+++ device_mecha.mk
@@ -183,6 +183,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/htc/mecha/modules/bcm4329.ko:system/lib/modules/bcm4329.ko
 
+$(call inherit-product, build/target/product/full_base.mk)
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_BRAND := HTC
 PRODUCT_NAME := htc_mecha
